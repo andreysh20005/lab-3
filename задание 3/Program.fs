76742be2d6b23d38ccc52ea1e7aca555deec9a33
andreysh20005 <andrejsemakin838@gmail.com>
2026-03-13 12:43:03 +0500

    Update Program.fs

--- задание 3/Program.fs
+++ задание 3/Program.fs
@@ -21,8 +21,12 @@ let main _ =
     let path = inputFilePath ()
     let filesSeq = 
         Directory.EnumerateFiles(path, "*")   
-        |> Seq.map upCase                      
-    let maxFileName = Seq.max filesSeq
-    printfn "файлы в указанной дирректории:\n %A" filesSeq
-    printfn "последнее по алфавиту имя: %s" maxFileName
+        |> Seq.map upCase 
+    if Seq.length filesSeq >0 then
+        let maxFileName = Seq.max filesSeq
+        printfn "файлы в указанной дирректории:\n %A" filesSeq
+        printfn "последнее по алфавиту имя: %s" maxFileName
+    else
+        printf "нет файлов в дирректории %s" path
+    
     0
